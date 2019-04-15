533b7156f55c272f775c7b730f8e9ab50ec4df2e
ryanpetersonOF <ryan@openfin.co>
2019-04-15 14:37:29 +0100

    [NA] Update typedoc title

--- src/cli.ts
+++ src/cli.ts
@@ -137,6 +137,6 @@ function generateTypedoc() {
         './dist/docs/api',
         './src/client/tsconfig.json'
     ].map(filePath => path.resolve(filePath));
-    const cmd = `"${typedocCmd}" --name "OpenFin ${config.SERVICE_NAME.charAt(0).toUpperCase() + config.SERVICE_NAME.slice(1)}" --theme "${themeDir}" --out "${outDir}" --excludeNotExported --excludePrivate --excludeProtected --hideGenerator --tsconfig "${tsConfig}" --readme none`; // eslint-disable-line
+    const cmd = `"${typedocCmd}" --name "OpenFin ${config.SERVICE_TITLE}" --theme "${themeDir}" --out "${outDir}" --excludeNotExported --excludePrivate --excludeProtected --hideGenerator --tsconfig "${tsConfig}" --readme none`; // eslint-disable-line
     childprocess.execSync(cmd, {stdio: 'inherit'});
 }
